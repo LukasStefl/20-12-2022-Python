ebe9f8a012d98e1fda3475460f735809b7bcbf5c
LukasStefl <lukas.stefl@student.ossp.cz>
2022-12-20 10:13:33 +0100

    commit

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ import os
 
 passwords = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-f = open("demofile3.txt", "w")
+f = open("chars.txt", "w")
 for x in passwords:
     f.write(x + '\n')
 f.close()
