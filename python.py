11830c0a32cdc6f51a563876aea53cf250531468
LukasStefl <lukas.stefl@student.ossp.cz>
2022-12-20 10:22:33 +0100

    commit

--- python.py
+++ python.py
@@ -1,12 +1,14 @@
 import os
 
 passwords = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+i = 0;
 f = open("chars.txt", "w")
-for x in passwords:
-    f.write(x)
-    f.write(x)
-    f.write('\n')
+for y in passwords:
+    for x in passwords:
+        f.write(x)
+        f.write(passwords[i])
+        f.write('\n')
+    i = i + 1
 f.close()
 
-os.remove("demofile.txt")
+#os.remove("chars.txt")
