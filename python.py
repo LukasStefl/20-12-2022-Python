40afeccd7faa2c78345d1cfc8c0d2a25dc7d9e70
LukasStefl <lukas.stefl@student.ossp.cz>
2022-12-20 11:29:59 +0100

    commit

--- python.py
+++ python.py
@@ -1,25 +1,25 @@
 import os
 
 passwords = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-i = 0;
+
 f = open("chars.txt", "w")
 for y in passwords:
     for x in passwords:
-        f.write(passwords[i])
+        f.write(y)
         f.write(x)
         f.write('diablo')
         f.write('\n')
 
         f.write('diablo')
-        f.write(passwords[i])
+        f.write(y)
         f.write(x)
         f.write('\n')
 
-        f.write(passwords[i])
+        f.write(y)
         f.write('diablo')
         f.write(x)
         f.write('\n')
-    i = i + 1
+
 
 f.close()
 
