cc6123daf080d8dd81f4a5e0dc5a860afd691e17
LukasStefl <lukas.stefl@student.ossp.cz>
2022-12-20 10:12:51 +0100

    commit

--- python.py
+++ python.py
@@ -1,15 +1,8 @@
 import os
-f = open("names.txt", "rt")
 
+passwords = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-d = f.read()
-w = d.split()
-
-for x in w:
-    w.count("Darth")
-    w.count("Luke")
-    w.count("Lea")
-
-print(w.count("Darth"))
-print(w.count("Luke"))
-print(w.count("Lea"))
+f = open("demofile3.txt", "w")
+for x in passwords:
+    f.write(x + '\n')
+f.close()
