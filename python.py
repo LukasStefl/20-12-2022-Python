7c0a1e3caeefb289ffa43fbb592f7eb8f1e8f268
LukasStefl <lukas.stefl@student.ossp.cz>
2022-12-20 11:02:05 +0100

    commit

--- python.py
+++ python.py
@@ -5,11 +5,26 @@ i = 0;
 f = open("chars.txt", "w")
 for y in passwords:
     for x in passwords:
+        f.write(passwords[i])
         f.write(x)
+        f.write('diablo')
+        f.write('\n')
+    i = i + 1
+
+for y in passwords:
+    for x in passwords:
+        
+        f.write('diablo')
+        f.write(passwords[i])
+        f.write(x)
+        f.write('\n')
+    i = i + 1
+
+for y in passwords:
+    for x in passwords:
         f.write(passwords[i])
         f.write('diablo')
         f.write(x)
-        f.write(passwords[i])
         f.write('\n')
     i = i + 1
 f.close()
